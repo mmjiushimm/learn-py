68caa9fdbf517f0f01d1916da49920f0e5503828
mingogo <im.mingogo@gmail.com>
2022-09-10 07:46:36 +0800

    print test

---
+++ basic/test_datatype.py
@@ -0,0 +1,16 @@
+# int
+print(100_000)
+print(0xa)
+
+# float
+print(1.2e5)
+
+# string
+print('i\'m a fish')
+print('\\\'\t\\')
+print(r'\\\t\\')
+print('''aaa
+bbb
+ccc''')
+print(r'''\\\n
+\\''')
