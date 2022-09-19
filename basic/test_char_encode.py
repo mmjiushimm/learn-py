772c28196144bf7ae10bdddd16d89e6249431af2
mingogo <im.mingogo@gmail.com>
2022-09-19 20:07:53 +0800

    print encode

--- basic/test_char_encode.py
+++ basic/test_char_encode.py
@@ -1,4 +1,10 @@
+# ord and chr
 print(ord('a'))
 print(ord('人'))
 print(chr(100))
 print(chr(10000))
+
+# encode and decode
+print('abc'.encode('ascii'))
+print('一二三'.encode('utf-8'))
+print('一二三'.encode('ascii'))
