e882d02ccb6ef823cae4cdaf927858894d0db30f
mingogo <im.mingogo@gmail.com>
2022-09-20 20:05:34 +0800

    add test_format.py

---
+++ basic/test_format.py
@@ -0,0 +1,8 @@
+# !/usr/bin/env/python3
+# -*- coding:utf-8 -*-
+
+print('%s' % 'abc')
+print('%06d' % 12)
+print('%.3f%%' % 1.20) # %% to print %
+print('%x' % 0xff)
+print('%s has %d books' % ('彤彤', 4))
