a45e433df78606a5ccd9bdad3bdb7d001dcd1684
mingogo <im.mingogo@gmail.com>
2022-09-20 19:44:18 +0800

    print decode

--- basic/test_char_encode.py
+++ basic/test_char_encode.py
@@ -1,3 +1,6 @@
+# usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 # ord and chr
 print(ord('a'))
 print(ord('人'))
@@ -5,6 +8,13 @@ print(chr(100))
 print(chr(10000))
 
 # encode and decode
-print('abc'.encode('ascii'))
-print('一二三'.encode('utf-8'))
+print('123 encode with utf-8 =', '123'.encode('utf-8'))
+print('abc encode with ascii =', 'abc'.encode('ascii'))
+print('abc encode with uft-8 =', 'abc'.encode('utf-8'))
+print('一二三 encode with utf-8 =', '一二三'.encode('utf-8'))
+print('abc一二三 encode with utf-8 =', 'abc一二三'.encode('utf-8'))
+bytes_encode_1 = '123'.encode('ascii')
+print(bytes_encode_1, "decode with utf-8 =", bytes_encode_1.decode())
+bytes_encode_2 = '一二三'.encode('utf-8')
+print(bytes_encode_2, "decode with utf-8 =", bytes_encode_2.decode('utf-8', errors = 'ignore'))
 print('一二三'.encode('ascii'))
